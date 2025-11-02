afb37ffab06efe9b5e996e7b6d38d7541624f74e
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-11-02 12:04:31 +0300

    feat(hero-section): implement HeroSection component

--- frontend/src/app/(routes)/(home)/page.tsx
+++ frontend/src/app/(routes)/(home)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '@/features/home/_components/HeroSection'
+import HomeContent from '@/features/home/_components/HomeContent'
 
 const HomePage = () => {
   return (
     <>
     <HeroSection />
+    <HomeContent />
     </>
   )
 }
--- frontend/src/features/home/_components/HeroSection.tsx
+++ frontend/src/features/home/_components/HeroSection.tsx
@@ -1,9 +1,11 @@
 import { Package, ShieldCheck, Smile } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const HeroSection = () => {
   return (
-    /* Navigation Menu Description */
+    <>
+    {/* Navigation Menu Description */}
     <div className="hidden md:flex md:px-5 flex-row xl:gap-24 py-2 items-center justify-center bg-gray-200 shadow-md">
       {/* Cargo Section */}
       <p className="flex flex-row items-center xl:gap-2 text-xs md:text-center">
@@ -32,7 +34,13 @@ const HeroSection = () => {
         <span>- 1.000.000+ MUTLU MÜŞTERİ</span>
       </p>
     </div>
-  );
+
+    {/* Hero Section Banner */}
+    <div>
+      <Image src="/images/hero-desktop-banner.png" alt="Hero Banner" width={1920} height={640} />
+    </div>
+    </>
+  )
 };
 
 export default HeroSection;
